5d956b966e9c0058cde3d47cc06ebe05636dbc6e
Arthur Sampaio <arthursampaio1611@gmail.com>
2023-10-23 23:31:39 -0300

    Correção bugs de casa finais (limitando um 'ponto' por numero)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
 import React, { useState } from "react";
+import "./App.css";
+
+const MenorNatural = (a, b) => {
+    if(a < 0){
+        return b;
+    }
+    if(b < 0){
+        return a;
+    }
+    
+    if(a < b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+const DetectOperator=(d)=>{
+    if(d == "+" || d == "/" || d == "*" || d == "-") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 export default function App() {
     const [valorTela, setValorTela] = useState("");
@@ -7,26 +32,19 @@ export default function App() {
 
     const Tela=(val, res)=>{
         return(
-            <div>
-                <span>{val}</span>{" = "}
-                <span style={{color:"#3aa"}}>{res}</span>
+            <div style={{fontSize:"20px", overflowX:"scroll", width:"400px", display:"flex"}}>
+                <span>
+                    <span>{val}</span>
+                    <span style={{color:"#3aa"}}>{" = "}{res}</span>
+                </span>
             </div>
         )
     }
 
     const Button=(label, onClick)=>{
         return(
-            <button onClick={onClick}>{label}</button>
+            <button style={{height:"100px", width:"100px", fontSize:"50px"}} onClick={onClick}>{label}</button>
         );
-    }
-
-    const DetectOperator=(d)=>{
-        if(d == "+" || d == "/" || d == "*" || d == "-") {
-            return true;
-        }
-        else {
-            return false;
-        }
     }
 
     const DetectaUltimo=()=>{
@@ -43,6 +61,24 @@ export default function App() {
 
 
     const addDigitoTela=(d) => {
+        if (d == ".") {
+            let valorTelaInvertido = valorTela.split("").reverse().join("");
+            let ultimaSoma = valorTelaInvertido.indexOf("+");
+            let ultimaSub = valorTelaInvertido.indexOf("-");
+            let ultimaMult = valorTelaInvertido.indexOf("*");
+            let ultimaDiv = valorTelaInvertido.indexOf("/");
+
+            let ultimafunc = MenorNatural(valorTela.length, ultimaSoma);
+            ultimafunc = MenorNatural(ultimafunc, ultimaSub);
+            ultimafunc = MenorNatural(ultimafunc, ultimaMult);
+            ultimafunc = MenorNatural(ultimafunc, ultimaDiv);
+
+            console.log(ultimaSoma, ultimaSub, ultimaDiv, ultimaMult, ultimafunc, valorTelaInvertido)
+            if (valorTelaInvertido.substring(0, ultimafunc).indexOf(".") != -1) {
+                return;
+            }
+        }
+
         if (DetectOperator(d)) {
             if (DetectaUltimo() == 1) {
                 let vtela=valorTela;
@@ -62,8 +98,7 @@ export default function App() {
                 return;   
             }
             setValorTela(valorTela + " " + d);
-        }
-        else {
+        } else {
             if (DetectaUltimo() == 1) {
                 setValorTela(valorTela + " " + d);
                 return;
@@ -104,7 +139,7 @@ export default function App() {
             stringDeletada = stringDeletada.substring(stringDeletada.length - 1, stringDeletada.length);
 
             if (stringDeletada == " ") {
-                vtela = vtela.substring(0, vtela.length - 3);
+                vtela = vtela.substring(0, vtela.length - 2);
                 setValorTela(vtela);
                 setOperacao(false);
                 return;
@@ -123,29 +158,31 @@ export default function App() {
     }
 
     return (
-        <>
+        <div style={{border:"1px solid", background:"#e1e1e1"}}>
             <h1>Calculadora</h1>
             {Tela(valorTela, resultado)}
-            {Button("AC", ()=>limparMemoria())}
-            {Button("(", ()=>addDigitoTela("("))}
-            {Button(")", ()=>addDigitoTela(")"))}
-            {Button("/", ()=>addDigitoTela("/"))}<br/>
-            {Button("7", ()=>addDigitoTela("7"))}
-            {Button("8", ()=>addDigitoTela("8"))}
-            {Button("9", ()=>addDigitoTela("9"))}
-            {Button("*", ()=>addDigitoTela("*"))}<br/>
-            {Button("4", ()=>addDigitoTela("4"))}
-            {Button("5", ()=>addDigitoTela("5"))}
-            {Button("6", ()=>addDigitoTela("6"))}
-            {Button("-", ()=>addDigitoTela("-"))}<br/>
-            {Button("1", ()=>addDigitoTela("1"))}
-            {Button("2", ()=>addDigitoTela("2"))}
-            {Button("3", ()=>addDigitoTela("3"))}
-            {Button("+", ()=>addDigitoTela("+"))}<br/>
-            {Button("0", ()=>addDigitoTela("0"))}
-            {Button(".", ()=>addDigitoTela("."))}
-            {Button("<-", ()=>Operacao("bs"))}
-            {Button("=", ()=>Operacao("="))}
-        </>
+            <div style={{display:"block"}}>
+                {Button("AC", ()=>limparMemoria())}
+                {Button("(", ()=>addDigitoTela("("))}
+                {Button(")", ()=>addDigitoTela(")"))}
+                {Button("/", ()=>addDigitoTela("/"))}<br/>
+                {Button("7", ()=>addDigitoTela("7"))}
+                {Button("8", ()=>addDigitoTela("8"))}
+                {Button("9", ()=>addDigitoTela("9"))}
+                {Button("*", ()=>addDigitoTela("*"))}<br/>
+                {Button("4", ()=>addDigitoTela("4"))}
+                {Button("5", ()=>addDigitoTela("5"))}
+                {Button("6", ()=>addDigitoTela("6"))}
+                {Button("-", ()=>addDigitoTela("-"))}<br/>
+                {Button("1", ()=>addDigitoTela("1"))}
+                {Button("2", ()=>addDigitoTela("2"))}
+                {Button("3", ()=>addDigitoTela("3"))}
+                {Button("+", ()=>addDigitoTela("+"))}<br/>
+                {Button("0", ()=>addDigitoTela("0"))}
+                {Button(".", ()=>addDigitoTela("."))}
+                {Button("<-", ()=>Operacao("bs"))}
+                {Button("=", ()=>Operacao("-"))}
+            </div>
+        </div>
     )
 }
